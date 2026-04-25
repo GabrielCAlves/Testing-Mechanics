b7ad31ae7415a9a39e6a3214bb5ba6f70b588ae5
Gabriel Cavalcanti Alves <gabriel0603@alu.ufc.br>
2026-04-25 19:28:31 -0300

    Updating Drag and Drop Scene
    - Added Drag function to the objects;
    - Added Shining Effect when the objects are clicked and holded;
    - Missing Drop function, at least for specific places;

---
+++ Assets/Scripts/6 - DragAndDropScripts/Shine.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using UnityEngine;
+
+public class Shine : MonoBehaviour
+{
+    [Header("Shine Settings")]
+    [SerializeField] private Material material;
+    [SerializeField] private Color shineColor;
+
+    [Header("Shine Effect Settings")]
+    [SerializeField] private float startIntensity = 0f;
+    [SerializeField] private float shineIntensity = 7f;
+    [SerializeField] private float shineDuration = 5f;
+    [SerializeField] private float chargeSpeed = .05f;
+
+    [Header("Debug")]
+    [SerializeField] private float elapsedTime = 0f;
+    [SerializeField] private float intensity = 0f;
+
+    void Start()
+    {
+        if(material == null)
+        {
+            material = GetComponent<Renderer>().material;
+            material.EnableKeyword("_EMISSION");
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if(material == null)
+        {
+            Debug.Log("Material not found on " + gameObject.name);
+            return;
+        }
+        
+        //material.SetColor("_EmissionColor", shineColor * shineIntensity);
+        StartCoroutine(ShineEffect(startIntensity, shineIntensity));
+    }
+
+    private void OnMouseUp()
+    {
+        if (material == null)
+        {
+            Debug.Log("Material not found on " + gameObject.name);
+            return;
+        }
+
+        StartCoroutine(ShineEffect(shineIntensity, startIntensity));
+    }
+
+    IEnumerator ShineEffect(float start, float end)
+    {
+        elapsedTime = 0f;
+        while (elapsedTime < shineDuration)
+        {
+            float t = elapsedTime / shineDuration; // Normalizes the time (0 to 1)
+            float smoothT = Mathf.SmoothStep(0, 1, t); // Applies easing
+            intensity = Mathf.Lerp(start, end, smoothT);
+            material.SetColor("_EmissionColor", shineColor * intensity);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        material.SetColor("_EmissionColor", shineColor * end);
+    }
+}
