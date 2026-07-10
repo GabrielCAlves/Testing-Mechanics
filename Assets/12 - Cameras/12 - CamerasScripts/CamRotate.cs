cb03db7f01a85ded70013b0532fff4ca6bf61931
Gabriel Cavalcanti Alves <gabriel0603@alu.ufc.br>
2026-07-10 17:55:55 -0300

    Configuring Real Game Shoot Style by manipulating the CamRotate script by allowing control over the permissions for X and Y changes in the axis

--- Assets/12 - Cameras/12 - CamerasScripts/CamRotate.cs
+++ Assets/12 - Cameras/12 - CamerasScripts/CamRotate.cs
@@ -4,6 +4,8 @@ public class CamRotate : MonoBehaviour
 {
     [SerializeField] private Vector2 turn;
     [SerializeField] private float sensitivity = .5f;
+    [SerializeField] private bool allowYAxis = true;
+    [SerializeField] private bool allowXAxis = true;
 
     // Update is called once per frame
     void Update()
@@ -11,6 +13,17 @@ public class CamRotate : MonoBehaviour
         turn.y += Input.GetAxis("Mouse Y") * sensitivity;
         turn.x += Input.GetAxis("Mouse X") * sensitivity;
 
-        transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
+        if(allowXAxis && allowYAxis)
+        {
+            transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
+        }
+        else if(allowYAxis)
+        {
+            transform.localRotation = Quaternion.Euler(0, turn.x, 0);
+        }
+        else if(allowXAxis)
+        {
+            transform.localRotation = Quaternion.Euler(-turn.y, 0, 0);
+        }
     }
 }
